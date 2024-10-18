3bf32a270742e48905744a222553f943343f2ee0
Your Name <you@example.com>
2024-10-18 14:00:18 +0530

    gallery and home done

--- Final-portfolio/js.js
+++ Final-portfolio/js.js
@@ -1,74 +1,70 @@
 let words = document.querySelectorAll(".word");
-words.forEach((word)=>{
-   let letters = word.textContent.split("");
-   word.textContent="";
-   letters.forEach((letter)=>{
-       let span = document.createElement("span");
-       span.textContent = letter;
-       span.className = "letter";
-       word.append(span);
-   });
+words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter) => {
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
 });
 
-let currentWordIndex = 0 ;
-let maxWordIndex = words.length -1 ;
-words[currentWordIndex].style.opacity ="1";
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
 
-let changeText = ()=>{
+let changeText = () => {
     let currentWord = words[currentWordIndex];
-    let nextWord = currentWordIndex === maxWordIndex ? words[0] :words[currentWordIndex + 1];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
-    Array.from(currentWord.children).forEach((letter,i)=>{
-        setTimeout(()=>{
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
             letter.className = "letter out";
-        },i * 80);
+        }, i * 80);
     });
-    nextWord.style.opacity="1";
-    Array.from(nextWord.children).forEach((letter, i)=>{
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
         letter.className = "letter behind";
-        setTimeout(()=>{
+        setTimeout(() => {
             letter.className = "letter in";
-        },340 + i * 80);
+        }, 340 + i * 80);
     });
     currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
 };
 
 changeText();
-setInterval(changeText,3000)
-
-
-
+setInterval(changeText, 3000)
 
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('nav ul li a');
 
 
-window.onscroll = ()=>{
-    sections.forEach(sec =>{
+window.onscroll = () => {
+    sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150 ;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >=offset && top < offset +height){
-                navLinks.forEach(links =>{
-                    links.classList.remove('active');
-                    document.querySelector('nav ul li a ' +
-                        '[href'+ id + ']').classList.add('active');
-                });
-        };
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('nav ul li a ' +
+                    '[href' + id + ']').classList.add('active');
+            });
+        }
+        ;
     });
 };
-
-
 
 
 // nav bar scrolled
 
 
 // JavaScript to show/hide the navigation bar based on scroll
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const nav = document.querySelector('nav');
     const bannerHeight = document.querySelector('.banner').offsetHeight;
 
@@ -78,3 +74,8 @@ window.addEventListener('scroll', function() {
         nav.classList.remove('scrolled'); // Remove class to hide nav
     }
 });
+
+
+
+
+
